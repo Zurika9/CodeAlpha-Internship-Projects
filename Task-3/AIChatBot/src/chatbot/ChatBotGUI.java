766fd89d6e40f1c0d3077b29a67c523c75b267ef
Bhavya Shukla <bhavyashukla560@gmail.com>
2026-07-08 19:23:49 +0530

    Built chatbot user interface layout

--- Task-3/AIChatBot/src/chatbot/ChatBotGUI.java
+++ Task-3/AIChatBot/src/chatbot/ChatBotGUI.java
@@ -8,12 +8,20 @@ import java.awt.*;
  */
 public class ChatBotGUI extends JFrame {
 
+    private JTextArea chatArea;
+    private JTextField inputField;
+    private JButton sendButton;
+    private JScrollPane scrollPane;
+    private JPanel bottomPanel;
+    private JLabel titleLabel;
     /**
      * Constructor
      */
     public ChatBotGUI() {
 
         initializeWindow();
+
+        initializeComponents();
 
         setVisible(true);
     }
@@ -34,5 +42,44 @@ public class ChatBotGUI extends JFrame {
         setResizable(false);
 
         getContentPane().setBackground(Color.WHITE);
+    }
+    private void initializeComponents() {
+
+        // Header
+        titleLabel = new JLabel("🤖 CodeAlpha AI ChatBot", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+
+        add(titleLabel, BorderLayout.NORTH);
+
+        // Chat Area
+        chatArea = new JTextArea();
+
+        chatArea.setEditable(false);
+
+        chatArea.setFont(new Font("Arial", Font.PLAIN, 16));
+
+        chatArea.setLineWrap(true);
+
+        chatArea.setWrapStyleWord(true);
+
+        scrollPane = new JScrollPane(chatArea);
+
+        add(scrollPane, BorderLayout.CENTER);
+
+        // Bottom Panel
+        bottomPanel = new JPanel(new BorderLayout());
+
+        inputField = new JTextField();
+
+        sendButton = new JButton("Send");
+
+        bottomPanel.add(inputField, BorderLayout.CENTER);
+
+        bottomPanel.add(sendButton, BorderLayout.EAST);
+
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        chatArea.append("Bot: Welcome! I am your AI ChatBot.\n\n");
+
     }
 }
